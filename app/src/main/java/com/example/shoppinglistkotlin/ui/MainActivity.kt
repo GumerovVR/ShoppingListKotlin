4a726ba7a7f1e3e7ff820c9710ed1ae3177bbf0e
Vladislav Gumerov <dev.gumerov@gmail.com>
2021-08-16 17:04:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglistkotlin/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglistkotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppinglistkotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
