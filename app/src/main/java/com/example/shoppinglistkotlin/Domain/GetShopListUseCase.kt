72c2394b0424a1d7d1159e7b577d365750c81f0c
Vladislav Gumerov <dev.gumerov@gmail.com>
2021-08-17 12:51:19 +0300

    Complete create use case and repo

---
+++ app/src/main/java/com/example/shoppinglistkotlin/Domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglistkotlin.Domain
+
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopList(): List<ShopItem> {
+        return shopListRepository.getShopList()
+    }
+}
